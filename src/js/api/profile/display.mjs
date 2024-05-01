6af1fa2a8a370317910d283ac6c2e5bccb744959
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-05-01 09:09:35 +0200

    fetching profile info

---
+++ src/js/api/profile/display.mjs
@@ -0,0 +1,35 @@
+import { API_SOCIAL } from "../constants.mjs";
+import { authFetch } from "../authFetch.mjs";
+import { profileInfo } from "../authFetch.mjs";
+
+const action = "/profiles";
+
+export async function displayProfiles() {
+  try {
+    const displayProfilesURL = `${API_SOCIAL}${action}`;
+
+    const response = await authFetch(displayProfilesURL);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function displayProfile(name) {
+  try {
+    if (!name) {
+      throw new Error("Get requires a profile name");
+    }
+    const displayProfileURL = `${API_SOCIAL}${action}/${name}`;
+
+    const response = await authFetch(displayProfileURL);
+
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(await displayProfile("fridafever"));
+const profile = profileInfo();
+console.log(profile.name);
