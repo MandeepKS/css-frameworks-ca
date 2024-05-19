195f6c3826ed2ecc05124beed3ed177f3b012ffa
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-05-19 22:25:13 +0200

    JSDocs add to backToTop

--- src/js/handlers/backToTopBtn.mjs
+++ src/js/handlers/backToTopBtn.mjs
@@ -1,3 +1,9 @@
+/**
+ * Sets up the back-to-top button.
+ * When clicked, the page will scroll to the top.
+ * If the button does not exist, the function will return.
+ */
+
 export function backToTopBtnHandler() {
   const backToTopBtn = document.querySelector(".back-to-top");
   if (!backToTopBtn) {
